c933430930e6fcbb6ab3b0687f5ba465311fc7b7
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-29 18:09:56 -0300

    Contato

--- app/parts/estrutura/footer.tsx
+++ app/parts/estrutura/footer.tsx
@@ -11,12 +11,11 @@ const Footer = () => {
         <footer className="bg-purple-700 py-8 md:py-12 block text-white p-4">
             <div className="container mx-auto px-4">
                 <div className="flex items-center flex-col">
-                    <h3 className="text-center text-xl md:text-3xl mb-4 font-playfair font-bold text-beige-500">Siga-nos no instagram</h3>
+                    <h3 className="text-center text-2xl md:text-3xl mb-4 font-playfair font-bold text-beige-500">Siga-nos no instagram</h3>
                     <Link href={ContentLinks.whatsapp}>
-                        <button type="button" className="w-full border border-zinc-200 text-white transition font-medium rounded-full my-2 py-3 px-8 flex justify-center items-center text-base gap-3">
-                            <FontAwesomeIcon icon={faInstagram} className="h-6 w-6" /> 
+                        <button type="button" className="w-full border border-zinc-200 text-white transition font-medium rounded-full my-2 py-3 px-8 flex justify-center items-center text-sm lg:text-base gap-3">
+                            <FontAwesomeIcon icon={faInstagram} className="h-4 md:h-6 w-4 md:w-6" /> 
                             <div> @projetomulheresdefrente</div>
-                            
                         </button>
                     </Link>
                 </div>
--- app/parts/home/contato.tsx
+++ app/parts/home/contato.tsx
@@ -11,8 +11,8 @@ const Contato = () => {
         <>
             <section id="contato" className="min-h-[35rem] py-4 md:py-20 bg-beige-500">
                 <div className="container mx-auto px-6">
-                    <div className="md:flex md:flex-row-reverse gap-x-4">
-                        <div className="md:w-5/12 text-sm md:text-xl rounded-xl">
+                    <div className="lg:flex lg:flex-row-reverse gap-x-4">
+                        <div className="lg:w-5/12 text-sm md:text-xl rounded-xl">
                             <div className="container mx-auto md:max-w-lg text-md md:text-lg p-4 md:p-6 my-3 md:mt-0">
                                 <div>
                                     <h3 className="text-2xl lg:text-3xl xl:text-4xl pb-6 text-center text-purple-700 font-playfair font-bold">Faça parte do projeto!</h3>
@@ -29,15 +29,15 @@ const Contato = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="md:w-7/12 text-sm md:text-xl text-black rounded-xl">
+                        <div className="lg:w-7/12 text-sm md:text-xl text-black rounded-xl">
                             <div className="relative text-md md:text-lg py-6 md:p-6 my-3 md:mt-0 h-full flex flex-col justify-center items-start">
                                 <div className="text-lg md:text-xl pb-8">
                                     <h3 className="font-medium text-purple-700 font-playfair text-3xl md:text-4xl: lg:text-4xl xl:text-5xl">Quem é <span className="font-bold">Daiane Luna</span>?</h3>
                                 </div>
-                                <div className="bg-purple-700 px-8 pt-8 rounded-3xl text-white text-sm md:text-base lg:text-xl pb-8 pr-16 md:pr-32 lg:pr-64 mr-6 md:mr-10 lg:mr-20">
+                                <div className="bg-purple-700 px-8 pt-8 rounded-3xl text-white text-base lg:text-lg xl:text-xl pb-8 pr-[38%] sm:pr-[25%] md:pr-40 lg:pr-52 xl:pr-64 md:mr-2 lg:mr-10 2xl:mr-20">
                                     <p className="font-light">Daiane Luna é uma líder visionária que promove o empoderamento econômico feminino para combater a violência, proporcionando educação, capacitação e recursos financeiros para mulheres vulneráveis no Rio de Janeiro.</p>
                                 </div>
-                                <Image src="/img/onu-transparente.png" className="absolute w-56 md:w-64 lg:w-96 -right-5 md:-right-10 -bottom-7 md:-bottom-20" width={383} height={629} alt="" />
+                                <Image src="/img/onu-transparente.png" className="absolute w-[45%] sm:w-[30%] md:w-38 lg:w-80 xl:w-96 -right-5 md:-right-10 -bottom-7 md:-bottom-24 lg:-bottom-20" width={383} height={629} alt="" />
                             </div>
                         </div>
                     </div>
