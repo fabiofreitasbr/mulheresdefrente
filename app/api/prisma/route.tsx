16612b0c5f076994e323da87d3e716dba8bc1f05
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-29 20:11:47 -0300

    Axios Post

--- app/api/prisma/route.tsx
+++ app/api/prisma/route.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
+
 export async function POST(req: Request, res: Response) {
-    const body = await req.json();
-    console.log(body);
-    return Response.json(body);
+    const { nome, email, telefone } = await req.json();
+    const responsePost = axios.post('https://infoback.infocomputec.com.br/mulheres/create', {nome, email, telefone})
+    return Response.json(responsePost);
 }
