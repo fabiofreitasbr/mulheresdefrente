09c6d107dfa89ed6412ded8ec4ae69b44419063d
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-29 20:03:42 -0300

    Campo

--- app/parts/estrutura/formContato.tsx
+++ app/parts/estrutura/formContato.tsx
@@ -13,7 +13,7 @@ export default function FormContato({ color = "white" }: colorType) {
     const [message, setMessage] = useState('');
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+    const [telefone, setTelefone] = useState('');
     const [colorMessage, setColorMessage] = useState('bg-red-700');
     const fieldColor = (color == "white") ? "bg-purple-700 md:bg-transparent bg-purple-700 border-zinc-200 placeholder:text-zinc-200 text-beige-500" : "bg-transparent border-purple-700 placeholder:text-gray-800 text-purple-700";
     const buttonColor = (color == "white") ? "bg-beige-500 hover:bg-beige-600 text-purple-700 " : "bg-purple-700 hover:bg-purple-600 text-beige-500 ";
@@ -24,19 +24,19 @@ export default function FormContato({ color = "white" }: colorType) {
     const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value);
     };
-    const handlePhone = (e: ChangeEvent<HTMLInputElement>) => {
-        setPhone(e.target.value);
+    const handleTelefone = (e: ChangeEvent<HTMLInputElement>) => {
+        setTelefone(e.target.value);
     };
     function onSubmitSend(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         if (nome == null || nome.length < 3) { setMessage('O nome digitado não é válido'); }
         else if (email == null || email.length < 5) { setMessage('O e-mail digitado não é válido'); }
-        else if (phone == null || phone.length < 7) { setMessage('O telefone digitado não é válido'); }
+        else if (telefone == null || telefone.length < 7) { setMessage('O telefone digitado não é válido'); }
         else if (!email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) { setMessage('O e-mail digitado não é válido'); }
         else {
             setMessage('');
-            axios.post('https://infoback.infocomputec.com.br/mulheres/create', { nome, email, phone })
+            axios.post('https://infoback.infocomputec.com.br/mulheres/create', { nome, email, Telefone })
             .then(function (response) {
                 if (response.data) { setMessage("Mensagem enviada com sucesso!"); setColorMessage('bg-green-700'); } else { setMessage("Houve um erro ao enviar a mensagem!"); setColorMessage('bg-red-700'); }
             })
@@ -48,7 +48,7 @@ export default function FormContato({ color = "white" }: colorType) {
             <div className="grid gap-5 container mx-auto max-w-lg">
                 <input type="text" name="nome" onChange={handleNome} value={nome} className={"w-full py-3 px-6 rounded-lg text-lg font-light border-2 " + fieldColor} placeholder="Nome" />
                 <input type="text" name="email" onChange={handleEmail} value={email} className={"w-full py-3 px-6 rounded-lg text-lg font-light border-2 " + fieldColor} placeholder="E-mail" />
-                <input type="text" name="phone" onChange={handlePhone} value={phone} className={"w-full py-3 px-6 rounded-lg text-lg font-light border-2 " + fieldColor} placeholder="Telefone" />
+                <input type="text" name="Telefone" onChange={handleTelefone} value={telefone} className={"w-full py-3 px-6 rounded-lg text-lg font-light border-2 " + fieldColor} placeholder="Telefone" />
                 <div className="flex justify-end">
                     <button type="submit" className={"py-2 px-6 text-base rounded-full transform hover:scale-105 duration-300 transition delay-300 uppercase flex justify-center items-center gap-x-2 " + buttonColor}><FontAwesomeIcon icon={faArrowRight} />Enviar</button>
                 </div>
