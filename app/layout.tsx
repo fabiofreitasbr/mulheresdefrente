c9b8198a440105a158d2fb24ddd378899e5404b6
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-30 14:33:11 -0300

    Script Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins, Playfair } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const playfair = Playfair({ subsets: ["latin"], weight: ["300", "500", "600", "700", "900"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "500", "700", "900"] });
@@ -17,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={playfair.className + " " + poppins.className + " antialiased"}>{children}</body>
+      <body className={playfair.className + " " + poppins.className + " antialiased"}>
+        
+                <Script id="google-analytics">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-GCSBGG19DD');
+                `}
+                </Script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GCSBGG19DD" />
+        {children}
+        </body>
     </html>
   );
 }
